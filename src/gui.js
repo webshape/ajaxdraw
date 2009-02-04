56fbf73bd3c7c0bdc40c150208d4034b841d5828
Piero Bizzotto <piero.bizzotto@gmail.com>
2009-02-04 18:17:23 +0100

    separato codice js GUI da index

---
+++ src/gui.js
@@ -0,0 +1,40 @@
+$(document).ready(function(){
+    
+    $("#example").dialog({
+    position: ["right","top"],
+    height: 300,
+    dialogClass: "Dialog1"
+    });
+    
+    $("#example2").dialog({
+    	position: ["right","bottom"]}
+    	);
+    
+    $("#picker").farbtastic("#color");
+    
+    $("#changeCol").click(function () {
+     		$("#colorx").show("slow"),
+         $(".Dialog1").height(500)
+      });
+     
+    $("#closeWheel").click(function () {
+     		$("#colorx").hide("slow"),
+     		$(".Dialog1").height(300);
+     	});
+     
+     <!-- Funzione gestione pulsante toolbar premuto/rilasciato -->
+     sentSelect=false;
+     $("#selezione").click(function () {
+		if(sentSelect==false){     
+       	$(this).css({"background-color":"#A0A0A0"}),
+       	sentSelect=true;
+       	}
+		else {
+			$(this).css({"background-color":"#F4F3F2"}),
+			sentSelect=false;     
+       }
+      });
+
+     
+     
+  });
