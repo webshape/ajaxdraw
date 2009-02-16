178f25d087f7abf8495ddb60eec68c17c4ffcb37
marco <marco@marco-laptop.(none)>
2009-02-16 11:49:13 +0100

    load: scheletro

---
+++ src/load.js
@@ -0,0 +1,15 @@
+/**
+ *	@fileoverview
+ * Save the content of the canvas in SVG format
+ * @author Marco Cunico
+ * @author Mirco Geremia
+ */
+
+
+/**
+ * SVGReader
+ */
+
+
+function SVGReader() {}
+
