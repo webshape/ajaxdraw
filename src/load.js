8747c4565e2f92ff0b254bfdcca54d5295d72fe6
Mirco Geremia <crittico@gmail.com>
2009-02-18 12:14:58 +0100

    load: parsing error

--- src/load.js
+++ src/load.js
@@ -1,6 +1,6 @@
 /**
  *	@fileoverview
- * Save the content of the canvas in SVG format
+ * Transform an SVG document in a FigureSet istance
  * @author Marco Cunico
  * @author Mirco Geremia
  */
@@ -8,8 +8,66 @@
 
 /**
  * SVGReader
+ * @constructor
  */
-
-
 function SVGReader() {}
 
+
+/**
+ * Create an instance of FigureSet parsing an SVG document
+ * @param {String} doc the SVG document
+ */
+SVGReader.prototype.read = function (doc) {
+  var psr = new XMLParser();
+  var xmlDoc = psr.parsing(doc);
+  if (xmlDoc == null)
+	 return 1; // parsing error
+
+  return 0;// xmlDoc contains the DOM of the SVG
+};
+
+
+/**
+ * XMLParser
+ * @constructor
+ */
+function XMLParser() {}
+
+
+/**
+ * Check if it is a well-formed document
+ * @param {String} doc the SVG document
+ */
+XMLParser.prototype.parsing = function (doc) {
+  try { //Internet Explorer
+	 var xmlDoc = new ActiveXObject("Microsoft.XMLDOM");
+	 xmlDoc.async = false;
+	 xmlDoc.loadXML(doc);
+
+	 if (xmlDoc.parseError.errorCode != 0) {
+		alert("Error in line " + xmlDoc.parseError.line +
+				" position " + xmlDoc.parseError.linePos +
+				"\nError Code: " + xmlDoc.parseError.errorCode +
+				"\nError Reason: " + xmlDoc.parseError.reason +
+				"Error Line: " + xmlDoc.parseError.srcText);
+		return(null);
+    }
+  }
+  catch(e) {
+	 try { //Firefox
+      var xmlDoc = document.implementation.createDocument("", "", null);
+		xmlDoc.async = false;
+		xmlDoc.loadXML(ddoc);
+		if (xmlDoc.documentElement.nodeName == "parsererror") {
+		  alert(xmlDoc.documentElement.childNodes[0].nodeValue);
+        return(null);
+      }
+    }
+	 catch(e) {alert(e.message);}
+  }
+  try {
+	 return (xmlDoc);
+  }
+  catch(e) {alert(e.message);}
+  return (null);
+};
