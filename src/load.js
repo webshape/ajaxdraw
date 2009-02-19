7ecf29b2eff01aaac62af323361b0c0ecd603018
Mirco Geremia <crittico@gmail.com>
2009-02-19 10:25:24 +0100

    load: ampliata struttura

--- src/load.js
+++ src/load.js
@@ -10,7 +10,10 @@
  * SVGReader
  * @constructor
  */
-function SVGReader() {}
+function SVGReader() {
+  var _fs = new FigureSet;
+  // TODO: create the hashtable through the register method
+}
 
 
 /**
@@ -25,12 +28,11 @@ SVGReader.prototype.read = function (doc) {
 
   var x = xmlDoc.getElementsByTagName("svg");
   for (var i = 0; i < x[0].childNodes.length; i++) {
-	 var n = x[0].childNodes[i].nodeName;
-	 if (n != "#text"){
-		document.write(n);
-	 }
-	 else {
-		document.write("<br/>");
+	 var n = x[0].childNodes[i];
+	 if (n.nodeName != "#text"){
+		var f = makeFigureClassFromTag(n.nodeName); // returns an instance of a figure
+		f.fromSVG(n);
+		_fs.push(f);
 	 }
   }
 
@@ -82,3 +84,25 @@ XMLParser.prototype.parsing = function (doc) {
   catch(e) {alert(e.message);}
   return (null);
 };
+
+
+
+/**
+ * Contain the hash table
+ * @constructor
+ */
+function SVGElementRegistry() {
+  //TODO: Hash table
+}
+
+
+/**
+ * Take a tag and return the corrispondent figure
+ * @param {String} tag the name of the tag
+ */
+SVGElementRegistry.prototype.makeFigureClassFromTag = function (tag) {
+  // TODO: check if tag is in the table and return the corrispondent figure
+};
+
+
+//TODO: every class fromSVG method 
